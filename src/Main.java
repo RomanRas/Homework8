f97311887edb255a1b9c5eb0d1a18b5e1715fd2c
RomanRas <kannidze@gmail.com>
2023-10-28 12:55:10 +1000

    Homework done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,81 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задача 1
+        System.out.println("Задача 1");
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] =3;
+
+        double[] floatArray = {1.57, 7.654, 9.986};
+
+        int[] randomArray = {2, 16, 54, -34, 15, 94, 15};
+
+        System.out.println();
+
+        //Задача 2
+        System.out.println("Задача 2");
+
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1) {
+                System.out.print(array[i]);
+            } else
+                System.out.print(array[i] + ", ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < floatArray.length; i++) {
+            if (i == floatArray.length - 1) {
+                System.out.print(floatArray[i]);
+            } else
+                System.out.print(floatArray[i] + ", ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < randomArray.length; i++) {
+            if (i == randomArray.length - 1) {
+                System.out.print(randomArray[i]);
+            } else
+                System.out.print(randomArray[i] + ", ");
+        }
+        System.out.println("\n");
+
+
+        //Задача 3
+        System.out.println("Задача 3");
+
+        for(int i = array.length -1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(array[i]);
+            } else
+                System.out.print(array[i] + ", ");
+        }
+        System.out.println();
+
+        for(int i = floatArray.length -1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(floatArray[i]);
+            } else
+                System.out.print(floatArray[i] + ", ");
+        }
+        System.out.println();
+
+        for(int i = randomArray.length -1; i >= 0; i--) {
+            if (i == 0) {
+                System.out.print(randomArray[i]);
+            } else
+                System.out.print(randomArray[i] + ", ");
+        }
+        System.out.println("\n");
+
+        //Задача 4
+        System.out.println("Задача 4");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                array[i] += 1;
+            }
+        }
+        System.out.println(Arrays.toString(array));
     }
 }
